e59a991cb05732da5e70c480ac73e19cf7b641e2
sunnidaze7 <pnykii98@gmail.com>
2023-07-03 21:14:48 -0500

    made static request

--- myApp.js
+++ myApp.js
@@ -1,13 +1,14 @@
 let express = require('express');
 let app = express();
 
-console.log('Hello World')
+
+app.get('/', (req, res)=>{
+    absolutePath = __dirname + '/views/index.html'
+    res.sendFile(absolutePath)
+})
 
 
-
-
-
-
+app.use("/public",express.static(__dirname + '/public'))
 
 
 
